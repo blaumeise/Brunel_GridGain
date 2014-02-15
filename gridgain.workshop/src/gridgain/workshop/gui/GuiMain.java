16503dd844e5034235d7fcb58a7c5235483e401a
Ana Fernandez <fernandez@recovecos.de>
2014-02-15 11:57:18 +0100

    Updated GUI

--- gridgain.workshop/src/gridgain/workshop/gui/GuiMain.java
+++ gridgain.workshop/src/gridgain/workshop/gui/GuiMain.java
@@ -1,21 +1,22 @@
 package gridgain.workshop.gui;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-public class gui extends JFrame {
-
-	public gui() {
-
+public class GuiMain extends JFrame {
+	public GuiMain() {
+		 setTitle("7zip password breaker");
+		 setSize(450, 150);
+		 setDefaultCloseOperation(EXIT_ON_CLOSE); 
 		initUI();
 	}
-
+	
 	private void initUI() {
-
+		
 		JPanel panel = new JPanel();
-		//panel.setSize(200, 200);
 		
 		getContentPane().add(panel);
 
@@ -23,19 +24,19 @@ public class gui extends JFrame {
 		panel.setToolTipText("Password Cracker");
 		panel.setBounds(0,100, 400, 100);
 
-		JButton btn_quit = new JButton("Quit");
+		JButton btn_quit = new JButton("Find the password");
 		btn_quit.setBounds(200, 60, 200, 30);
-		btn_quit.setToolTipText("Quit");
+		btn_quit.setToolTipText("Start finding the password for the given file");
 
 		panel.add(btn_quit);
 
-		JButton btn_ok = new JButton("Start search for password");
+		JButton btn_ok = new JButton("File...");
 		btn_ok.setBounds(200, 20, 200, 30);
-		btn_ok.setToolTipText("Start search for password");
+		btn_ok.setToolTipText("Choose a 7zip file");
 
 		panel.add(btn_ok);
 
-		JTextField txt_input = new JTextField("Choose file");
+		JTextField txt_input = new JTextField("");
 		txt_input.setBounds(0, 20, 200, 30);
 		
 		panel.add(txt_input);
@@ -52,9 +53,10 @@ public class gui extends JFrame {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				gui ex = new gui();
+				GuiMain ex = new GuiMain();
 				ex.setVisible(true);
 			}
 		});
 	}
+
 }
