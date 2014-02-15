6032161b2a1752417a061029b93e735d6f68a4cf
Ana Fernandez <fernandez@recovecos.de>
2014-02-15 13:27:21 +0100

    GUI updated: added file chooser

--- gridgain.workshop/src/gridgain/workshop/gui/GuiMain.java
+++ gridgain.workshop/src/gridgain/workshop/gui/GuiMain.java
@@ -1,15 +1,25 @@
 package gridgain.workshop.gui;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.SwingUtilities;
 
 public class GuiMain extends JFrame {
+	private JTextField txt_input; 
+	private JTextPane txt_output;
+	
 	public GuiMain() {
 		 setTitle("7zip password breaker");
-		 setSize(450, 150);
+		 setSize(450, 170);
 		 setDefaultCloseOperation(EXIT_ON_CLOSE); 
 		initUI();
 	}
@@ -22,31 +32,50 @@ public class GuiMain extends JFrame {
 
 		panel.setLayout(null);
 		panel.setToolTipText("Password Cracker");
-		panel.setBounds(0,100, 400, 100);
+		panel.setBounds(10,100, 400, 100);
 
 		JButton btn_quit = new JButton("Find the password");
-		btn_quit.setBounds(200, 60, 200, 30);
+		btn_quit.setBackground(Color.ORANGE);
+		btn_quit.setOpaque(true);
+		btn_quit.setBounds(210, 60, 200, 30);
 		btn_quit.setToolTipText("Start finding the password for the given file");
+		
 
 		panel.add(btn_quit);
 
-		JButton btn_ok = new JButton("File...");
-		btn_ok.setBounds(200, 20, 200, 30);
-		btn_ok.setToolTipText("Choose a 7zip file");
+		JButton btn_file = new JButton("File...");
+		btn_file.setBounds(210, 20, 200, 30);
+		btn_file.setToolTipText("Choose a 7zip file");
+		btn_file.addActionListener(new OpenL());
 
-		panel.add(btn_ok);
+		panel.add(btn_file);
 
-		JTextField txt_input = new JTextField("");
-		txt_input.setBounds(0, 20, 200, 30);
+		txt_input = new JTextField("");
+		txt_input.setBounds(10, 20, 200, 30);
 		
 		panel.add(txt_input);
 
-		JTextField txt_output = new JTextField("Receive Password here");
-		txt_output.setBounds(0, 60, 200, 30);
+		txt_output = new JTextPane();
+		txt_output.setBounds(10, 100, 400, 30);
 		
 		panel.add(txt_output);
 
 	}
+	
+	  class OpenL implements ActionListener {
+		    public void actionPerformed(ActionEvent e) {
+		      JFileChooser c = new JFileChooser();
+		      int rVal = c.showOpenDialog(GuiMain.this);
+		      if (rVal == JFileChooser.APPROVE_OPTION) {
+		    	  txt_input.setText(c.getSelectedFile().getName());
+		    	  txt_output.setText(c.getSelectedFile().getName() + " selected!");
+		        //...
+		      }
+		      if (rVal == JFileChooser.CANCEL_OPTION) {
+		    	  txt_output.setText("No file selected!");
+		      }
+		    }
+		  }
 
 	public static void main(String[] args) {
 
